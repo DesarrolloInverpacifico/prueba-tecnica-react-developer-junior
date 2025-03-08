e04484d514da7d87530ae00f0f0a9253f5d43842
eiris elias <eiriselias@gmail.com>
2025-03-08 13:38:22 -0500

    se organizo la pagina de detalles segun el diseño

--- pruebaTecnica/src/components/Description.tsx
+++ pruebaTecnica/src/components/Description.tsx
@@ -6,6 +6,8 @@ import Table from "./Table"
 import { useSEO } from "../hooks/useSEO"
 import { calculateWorkHours, ICalculo } from "../helpers/calculates"
 import { columnsDescription } from "../helpers/colums"
+import Footer from "./Footer"
+import NavBar from "./NavBar"
 
 function Description() {
     
@@ -16,11 +18,16 @@ function Description() {
     const [dataAccess, setDataAccess] = useState<IAccessControls[]>([])
     const [processedData, setProcessedData] = useState<IAccessControls[]>([]);
     const [calculo, setCalculo] = useState<ICalculo | null>(null)
+    const [show, setShow] = useState(false)
     useSEO(`${data?.attributes.first_name} - detalles`, `Vista de detalles del usuario ${data?.attributes.first_name} ${data?.attributes.last_name}`)
 
-    const volver = ()=>{
+    const back = ()=>{
         navigate("/")
     }
+
+    const showSesion = ()=>{
+        setShow(!show)
+    }   
     
       useEffect(()=>{
         const fetchUser = async()=>{
@@ -70,39 +77,66 @@ function Description() {
    
   return (
     <div>
-        <h1 className="flex flex-col justify-center items-center font-bold text-4xl my-8 text-yellow-300"> 
-            {data?.attributes.first_name.toLocaleUpperCase()} {data?.attributes.last_name.toLocaleUpperCase()} </h1>    
+        <NavBar/>
         {
             calculo && ( 
-            <div className="flex justify-between mb-4 mx-16">
-                <div className="flex flex-col justify-center items-center font-bold">
-                    <h2>Horas trabajadas {Math.floor(calculo.totalWorkedHours)}</h2>
-                    <h2>Hora ordinarias {Math.floor(calculo.totalOrdinaryHours)} </h2>
-                    <h2>Horas extras {Math.floor(calculo.totalOvertimeHours)}</h2> 
-                    <h2>Horas recargos {Math.floor(calculo.totalRecaudeHours)}</h2>
+
+            <div>
+                <header className="relactive">
+                    <img src="/payFondo.png" alt="payFondo" className="w-full h-[80vh]" />
+                    <h1 className="absolute left-40 top-30 text-4xl font-bold text-blue-700">{data?.attributes.first_name.toLocaleUpperCase()}</h1>
+                    <h2 className="absolute left-50 top-40 text-3xl font-bold text-blue-700">{data?.attributes.last_name.toLocaleUpperCase()}</h2>
+                    <h2 className="absolute left-44 top-54 text-2xl font-bold text-blue-700">{data?.attributes.charge.toLocaleUpperCase()}</h2>
+                    <h2 className="absolute right-80 bottom-80 text-3xl font-bold text-blue-700">Salario Total</h2>
+                    <h2 className="absolute right-80 bottom-70 text-3xl font-bold bg-blue-700 text-amber-300 px-8">${Math.floor(calculo.totalSalary).toLocaleString('es-CO')}</h2>
+                    <button 
+                        onClick={back}
+                        className="absolute right-80 bottom-40 font-bold bg-blue-700 text-amber-300 px-8 py-2 rounded-full"
+                    >
+                        Volver
+                    </button>
+                </header> 
+                <section className="grid grid-cols-2 items-center justify-center mt-8">
+                    <div className="flex items-center justify-center">
+                        <img src="/logo.png" alt="logo" className="w-[40%] h-40%"/>
+                    </div>
+                    <div>
+                        <h2 className="text-2xl text-blue-700">Salario Basico: ${Math.floor(calculo.basySalary).toLocaleString('es-CO')}</h2>
+                        <h2 className="font-bold text-3xl text-blue-700">Horas trabajadas {Math.floor(calculo.totalWorkedHours)}</h2>
+                        <h2 className="text-3xl text-blue-700">Hora ordinarias {Math.floor(calculo.totalOrdinaryHours)} </h2>
+                        <h2 className="text-3xl text-blue-700">Horas extras {Math.floor(calculo.totalOvertimeHours)}</h2> 
+                        <h2 className="text-3xl text-red-500">Horas recargos {Math.floor(calculo.totalRecaudeHours)}</h2>
+                        <h2 className="font-bold text-3xl text-blue-700 mt-8 ml-16">Salario Total: 
+                            <span className="ml-2 text-3xl font-bold bg-blue-700 text-amber-300 px-8">${Math.floor(calculo.totalSalary).toLocaleString('es-CO')}</span> 
+                        </h2>
+                    </div>
+                </section>  
+                <section className="bg-blue-700 grid grid-cols-2 items-center justify-items-center justify-center mt-8 p-8">
+                    <div className="text-amber-400 flex flex-col gap-2 font-bold text-2xl">
+                        {calculo.overtimeBreakdown.HED >= 0 && <p>Horas extras diurnas: {Math.floor(calculo.overtimeBreakdown.HED)}</p>}
+                        {calculo.overtimeBreakdown.HEN >= 0 && <p>Horas extras nocturnas: {Math.floor(calculo.overtimeBreakdown.HEN)}</p>}
+                        {calculo.overtimeBreakdown.HEDD >= 0 && <p>Hora extras diurna dominical o festiva: {Math.floor(calculo.overtimeBreakdown.HEDD)}</p>}
+                        {calculo.overtimeBreakdown.HEDN >= 0 && <p>Hora extra dominical nocturna: {Math.floor(calculo.overtimeBreakdown.HEDN)}</p>}
+                        {calculo.overtimeBreakdown.RC >= 0 && <p>Recargo nocturno: {Math.floor(calculo.overtimeBreakdown.RC)}</p>}
+                        {calculo.overtimeBreakdown.RD >= 0 && <p>Recargo dominical: {Math.floor(calculo.overtimeBreakdown.RD)}</p>}
+                        {calculo.overtimeBreakdown.RND >= 0 && <p>Recargo nocturno dominical: {Math.floor(calculo.overtimeBreakdown.RND)}</p>}
+                    </div>
+                    <div>
+                        <img src="/reloj.png" alt="iconoReloj" width={400}/>
+                    </div>
+                </section>
+                <div className="flex flex-col justify-center items-center text-2xl text-blue-700 p-16 font-bold">
+                    <button onClick={showSesion} className="cursor-pointer">{show ? "Ocultar": "Ver Detalles"}</button>
                 </div>
-                <div className="flex flex-col justify-center items-center">
-                    {calculo.overtimeBreakdown.HED >= 0 && <p>Horas extras diurnas: {Math.floor(calculo.overtimeBreakdown.HED)}</p>}
-                    {calculo.overtimeBreakdown.HEN >= 0 && <p>Horas extras nocturnas: {Math.floor(calculo.overtimeBreakdown.HEN)}</p>}
-                    {calculo.overtimeBreakdown.HEDD >= 0 && <p>Hora extras diurna dominical o festiva: {Math.floor(calculo.overtimeBreakdown.HEDD)}</p>}
-                    {calculo.overtimeBreakdown.HEDN >= 0 && <p>Hora extra dominical nocturna: {Math.floor(calculo.overtimeBreakdown.HEDN)}</p>}
-                    {calculo.overtimeBreakdown.RC >= 0 && <p>Recargo nocturno: {Math.floor(calculo.overtimeBreakdown.RC)}</p>}
-                    {calculo.overtimeBreakdown.RD >= 0 && <p>Recargo dominical: {Math.floor(calculo.overtimeBreakdown.RD)}</p>}
-                    {calculo.overtimeBreakdown.RND >= 0 && <p>Recargo nocturno dominical: {Math.floor(calculo.overtimeBreakdown.RND)}</p>}
-                    
-                </div>
-                <div className="flex  flex-col justify-center items-center">
-                    <h2 className="font-bold">Salario Basico: ${Math.floor(calculo.basySalary).toLocaleString('es-CO')}</h2>
-                    <h2 className="font-bold text-2xl">Salario Total: $<span className="text-yellow-300">{Math.floor(calculo.totalSalary).toLocaleString('es-CO')}</span></h2>
-                    <button className="py-4 px-8 font-bold bg-yellow-300 mt-4 text-black rounded-2xl hover:bg-yellow-200 hover:cursor-pointer"
-                    onClick={volver}
-                    >Volver</button>
-                </div>
+                
                 
             </div>
             )
         }                                  
-                <Table data={processedData} columns={columnsDescription} />            
+                <section id="descriptionTable" className={show ? '':"hidden"}>
+                    <Table data={processedData} columns={columnsDescription} />            
+                </section>
+                <Footer/>
     </div>
   )
 }
